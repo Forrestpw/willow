32cf2d7611bcc0cf89a2665dff9ccf1b4e21d874
Forrest Walker <forrestpatwalker@hotmail.com>
2024-01-16 14:32:39 -0700

    add MainLayout for common functionality between pages

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,12 @@
+<template>
+    <Link href="/">Main Page</Link>&nbsp;
+    <Link href="/hello">Show Page</Link>
+
+    <slot>
+        Default
+    </slot>
+</template>
+
+<script setup>
+    import { Link } from '@inertiajs/inertia-vue3'
+</script>

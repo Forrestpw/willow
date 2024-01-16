78dd90abeca1b2b49ee77c909384bec72cf18a4c
Forrest Walker <forrestpatwalker@hotmail.com>
2024-01-16 14:43:58 -0700

    add ref timer and display the time the user has been on the page

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,6 +1,7 @@
 <template>
     <Link href="/">Main Page</Link>&nbsp;
     <Link href="/hello">Show Page</Link>
+    <div>The page with time {{ timer }}</div>
 
     <slot>
         Default
@@ -9,4 +10,8 @@
 
 <script setup>
     import { Link } from '@inertiajs/inertia-vue3'
+    import { ref } from 'vue'
+
+    const timer = ref(0)
+    setInterval(() => timer.value++, 1000)
 </script>
